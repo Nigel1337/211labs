04997c862db2c13783edab41bfad236e2bf27a5f
KingG <KingG@137.125.156.222>
2022-11-03 14:17:40 -0400

    Upload

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+/* doesn't work with source level 1.8:
+module com.mycompany.mavenproject3 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens com.mycompany.mavenproject3 to javafx.fxml;
+    exports com.mycompany.mavenproject3;
+}
+*/
